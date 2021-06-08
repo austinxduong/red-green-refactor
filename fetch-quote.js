8b958c5d20f06f6e206799d07a71cf24ad871ff3
austinxduong <axduong2@gmail.com>
2021-06-08 10:57:36 -0700

    install missing npm node-fetch finally passe fetchQuotes function

--- fetch-quote.js
+++ fetch-quote.js
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
 
 export async function fetchQuotes(number) {
-  const response = await fetch('futuramaapi.herokuapp.com/api/quotes');
-
+  const response = await fetch(`http://futuramaapi.herokuapp.com/api/quotes/${number}`);
+  // console.log(await response.json());
+  
   const quote = await response.json();
   return {
     name: quote[0].character,
